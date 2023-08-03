f38c97d003be2cf33330af69759afd831591f845
thomasbush0909 <129676598+thomasbush0909@users.noreply.github.com>
2023-08-03 19:02:56 +0300

    push main

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+ 
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s x\n", argv[0]);
+        exit(1);
+    }
+
+    double x = atof(argv[1]);
+    double result = sqrt(x);
+    
+    int r = 0;
+
+    if(x < 0) {
+        fprintf(stderr, "Cannot handle complex roots\n");
+        exit(2);
+    }
+
+    printf("square root of %f = %f\n", x, result);
+
+    return 0;
+
+}
